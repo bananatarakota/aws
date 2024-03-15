60ba0f1adf182ed27427ed3e74e1f24803206c43
Aglaya Khalipskaya <aglaya.khalipskaya@estudiantat.upc.edu>
2024-03-15 07:14:16 +0000

    aaa

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,5 @@
 //= link_directory ../stylesheets .css
 
 //= require_tree ./channels
-//= require engine_name/application.js 
-//= require_directory ../../vendors/DataTables
-//= require_tree ../../../vendor/assets/javascripts/onepager
+
+
