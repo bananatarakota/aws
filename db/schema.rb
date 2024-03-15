17cfa1b3aeec1d74722564cba744571580831d8f
Aglaya Khalipskaya <aglaya.khalipskaya@estudiantat.upc.edu>
2024-03-15 07:56:24 +0000

    bbb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_13_140948) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_14_225322) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tweets", force: :cascade do |t|
     t.string "author"
     t.text "content"
